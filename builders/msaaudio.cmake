f58ac75d555c4ecad74ad9663ed3dfe1777c5192
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-01-14 11:20:57 +0100

    Added required changes for AAudio

---
+++ builders/msaaudio.cmake
@@ -0,0 +1,34 @@
+############################################################################
+# msaaudio.cmake
+# Copyright (C) 2019  Belledonne Communications, Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+############################################################################
+
+lcb_git_repository("https://gitlab.linphone.org/BC/public/msaaudio.git")
+lcb_git_tag_latest("master")
+lcb_git_tag("master")
+lcb_external_source_paths("msaaudio")
+lcb_groupable(YES)
+lcb_sanitizable(YES)
+lcb_plugin(YES)
+
+lcb_dependencies("ms2")
+lcb_cmake_options(
+        "-DENABLE_AAUDIO=${ENABLE_AAUDIO}"
+)

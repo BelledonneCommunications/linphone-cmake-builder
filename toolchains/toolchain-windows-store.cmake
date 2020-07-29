4708f4d4b081d64510f03b674786f1a6e5d3721f
Julien Wadel <julien.wadel@belledonne-communications.com>
2020-07-29 18:48:27 +0200

    Add definition and constraint SAFESEH to be not for Debug build

--- toolchains/toolchain-windows-store.cmake
+++ toolchains/toolchain-windows-store.cmake
@@ -29,6 +29,7 @@ if(POLICY CMP0081)
 	cmake_policy(SET CMP0081 OLD)#Needed to set LINK_DIRECTORIES with Visual Studio variables
 endif ()
 set(ENABLE_MICROSOFT_STORE_APP YES)
+add_definitions(-DENABLE_MICROSOFT_STORE_APP)
 set(MICROSOFT_STORE_LINK_PATHS "\$(WindowsSDK_LibraryPath_x86);\$(NETFXKitsDir)Lib\\um\\x86;\$(VC_LibraryPath_VC_x86_store);\$(VC_ReferencesPath_ATL_x86);\$(VC_LibraryPath_VC_x86);\$(VC_LibraryPath_x86);\$(VC_VS_LibraryPath_VC_VS_x86);\$(LibraryPath);\$(VC_LibraryPath_VC_x86_store)\\references")
 
 if(NOT LINPHONE_ADDLIBRARY_DEFINED)

582f88ecc672ce45e93bc297e5ec09aa32e87c2b
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2020-08-12 15:30:07 +0200

    Remove the ENABLE_REGEVENT booleans, only ENABLE_CONFERENCE is needed now

--- options/flexisip.cmake
+++ options/flexisip.cmake
@@ -25,7 +25,6 @@
 lcb_add_option("Redis" "Enable hiredis support." "${DEFAULT_VALUE_ENABLE_REDIS}")
 lcb_add_option("Presence" "Enable presence server support." "${DEFAULT_VALUE_ENABLE_PRESENCE}")
 lcb_add_option("Conference" "Enable conference server support." "${DEFAULT_VALUE_ENABLE_CONFERENCE}")
-lcb_add_option("Reg Event" "Enable regevent server support." "${DEFAULT_VALUE_ENABLE_REGEVENT}")
 lcb_add_option("SNMP" "Enable SNMP support." "${DEFAULT_ENABLE_SNMP}")
 lcb_add_option("Transcoder" "Enable transcoder support." "${DEFAULT_VALUE_ENABLE_TRANSCODER}")
 lcb_add_option("Protobuf" "Enable protobuf for REDIS." "${DEFAULT_ENABLE_PROTOBUF}" "ENABLE_PROTOBUF" OFF)

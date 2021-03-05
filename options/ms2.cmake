972f7cf52f5e2981a6dd0eb886a2483a7c3d7aaa
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-03-05 18:33:17 +0100

    Remove libyuv

--- options/ms2.cmake
+++ options/ms2.cmake
@@ -66,7 +66,6 @@ lcb_add_option("MKV" "MKV playing and recording support." "${DEFAULT_VALUE_ENABL
 lcb_add_strict_dependent_option("MPEG4" "MPEG4 video encoding/decoding support (require license)." OFF "ENABLE_FFMPEG" OFF "ENABLE_NON_FREE_CODECS" "non free codecs option not enabled (ENABLE_NON_FREE_CODECS).")
 lcb_add_strict_dependent_option("OpenH264" "H.264 video encoding/decoding support with the openh264 library (require license)." OFF "ENABLE_VIDEO" OFF "ENABLE_NON_FREE_CODECS" "non free codecs option not enabled (ENABLE_NON_FREE_CODECS).")
 lcb_add_dependent_option("VPX" "VPX (VP8) video encoding/decoding support." "${DEFAULT_VALUE_ENABLE_VPX}" "ENABLE_VIDEO" OFF)
-lcb_add_dependent_option("LibYUV" "Enable LibYUV support" "${DEFAULT_VALUE_ENABLE_LIBYUV}" "ENABLE_VIDEO" OFF)
 lcb_add_strict_dependent_option("X264" "H.264 video encoding support with the x264 library (require license)." OFF "ENABLE_VIDEO;ENABLE_GPL_THIRD_PARTIES;ENABLE_UNMAINTAINED" OFF "ENABLE_NON_FREE_CODECS" "non free codecs option not enabled (ENABLE_NON_FREE_CODECS).")
 
 set(CMAKE_ABOVE_16 False)

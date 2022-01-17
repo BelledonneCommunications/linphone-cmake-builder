4c4c84bcf3b0116daac05fa50bd09aacdb1f01ee
Clemence Him <clemence.him@belledonne-communications.com>
2022-01-17 18:02:47 +0100

    Change sidh library

--- builders/sidh.cmake
+++ builders/sidh.cmake
@@ -2,10 +2,10 @@
 # sidh.cmake
 ############################################################################
 
-lcb_git_repository("https://gitlab.linphone.org/BC/private/pqcrypto-sidh.git")
+lcb_git_repository("https://github.com/open-quantum-safe/liboqs.git")
 lcb_git_tag_latest("master")
 lcb_git_tag("master")
-lcb_external_source_paths("external/sidh")
+lcb_external_source_paths("external/liboqs")
 lcb_groupable(YES)
 lcb_sanitizable(YES)
 lcb_package_source(YES)

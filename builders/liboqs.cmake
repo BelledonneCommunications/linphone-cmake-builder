81845eaed4160a98428ceb964c95024cb724de0f
Clemence Him <clemence.him@belledonne-communications.com>
2022-01-21 15:14:45 +0100

    Build with static lib

--- builders/liboqs.cmake
+++ builders/liboqs.cmake
@@ -10,6 +10,23 @@ lcb_groupable(YES)
 lcb_sanitizable(YES)
 lcb_package_source(YES)
 lcb_spec_file("liboqs.spec")
-lcb_cmake_options("-DOQS_MINIMAL_BUILD="OQS_KEM_alg_sike_p434;OQS_KEM_alg_sike_p434_compressed;OQS_KEM_alg_sike_p503;OQS_KEM_alg_sike_p503_compressed;OQS_KEM_alg_sike_p610;OQS_KEM_alg_sike_p610_compressed;OQS_KEM_alg_sike_p751;OQS_KEM_alg_sike_p751_compressed""
-    "-DOQS_DIST_BUILD=OFF"
-    "-DOQS_USE_OPENSSL=OFF")
+lcb_cmake_options("-DOQS_DIST_BUILD=ON" "-DOQS_USE_OPENSSL=OFF")
+lcb_cmake_options("-DBUILD_SHARED_LIBS=Off") # TODO: this force the generation of static lib while we should translate the ENABLE_SHARED/ENABLE_STATIC
+# Note: -DOQS_MINIMAL_BUILD is what we want to use but a ; separated list cannot go through cmake-builder
+# so use the -DOQS_ENABLE_<family> and turn them all Off except the one we want. Sike and Kyber are explicitely turned on but it is useless
+lcb_cmake_options("-DOQS_ENABLE_SIG_SPHINCS=Off"
+	"-DOQS_ENABLE_SIG_RAINBOW=Off"
+	"-DOQS_ENABLE_SIG_FALCON=Off"
+	"-DOQS_ENABLE_SIG_DILITHIUM=Off"
+	"-DOQS_ENABLE_KEM_SABER=Off"
+	"-DOQS_ENABLE_KEM_NTRUPRIME=Off"
+	"-DOQS_ENABLE_KEM_NTRU=Off"
+	"-DOQS_ENABLE_KEM_HQC=Off"
+	"-DOQS_ENABLE_KEM_CLASSIC_MCELIECE=Off"
+	"-DOQS_ENABLE_SIG_PICNIC=Off"
+	"-DOQS_ENABLE_KEM_SIDH=On"
+	"-DOQS_ENABLE_KEM_FRODOKEM=Off"
+	"-DOQS_ENABLE_KEM_BIKE=Off"
+	"-DOQS_ENABLE_KEM_SIKE=On"
+	"-DOQS_ENABLE_KEM_KYBER=On"
+        )
